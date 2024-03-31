ecde6cb266a160812761f73d3188485d193f6307
Ayush Kumar <ayush.kumar1@juspay.in>
2024-03-31 15:50:22 +0530

    data_fill_1.0.1:2024-03-31_15:50:17

--- prerelease.sh
+++ prerelease.sh
@@ -1,4 +1,4 @@
 current_date=$(date +"%Y-%m-%d_%H:%M:%S")
 branch=$(git symbolic-ref --short HEAD)
 
-rm -rf docs && npm run build && cp -r dist docs && git add . && git commit -m "$branch_$current_date" && git push origin $branch
+rm -rf docs && npm run build && cp -r dist docs && git add . && git commit -m "$branch:$current_date" && git push origin $branch
